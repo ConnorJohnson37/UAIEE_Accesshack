ac987aba63a16274e83dcf03270a8f276ab784e2
Bri <bcurley46@gmail.com>
2023-07-21 20:05:34 -0700

    configuring api auth

--- main.py
+++ main.py
@@ -2,5 +2,17 @@
 #Bri was here :)
 #Arj is here
 
+from __future__ import print_function
+import time
+import swagger_client
+from swagger_client.rest import ApiException
+from pprint import pprint
+
+#Set up API authentication using key
+configuration = swagger_client.Configuration()
+configuration.api_key['key'] = '49880dc5499b46fa967202042232107'
+
+#Create class
+
 print("Hello World")
 print("bruh")
