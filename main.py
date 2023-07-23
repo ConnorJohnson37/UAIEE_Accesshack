99ddc5632bcf8fc66e816d2e32e723be6e81a65e
Connor <cmjagent@gmail.com>
2023-07-22 23:03:45 -0700

    introduced pyscript to the world

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import cgi
 form = cgi.FieldStorage()
 city =  form.getvalue('entercity') #get city entered by user in index.html
 days = 7 #show 7 day forcast
-dt = '2023-07-22' #date FIXME set to today's date
+dt = '2023-07-23' #date FIXME set to today's date
 unixdt = 56 # int | Please either pass 'dt' or 'unixdt' and not both in same request. unixdt should be between today and next 14 day in Unix format. e.g. 1490227200  (optional)
 lang = 'lang_example' #language
 
